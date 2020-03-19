7a90c5b6a34959cb16fec51768cd32cd7cd70e4d
ashishgeorge009 <ashgeroge009@gmail.com>
2020-03-19 14:34:19 +0530

    port

--- server.js
+++ server.js
@@ -13,7 +13,7 @@ io.on('connection',function(socket){
     });
 });
 
-const port = process.env.port||3000
+const port = process.env.PORT||3000
 
  server.listen(port,function(req,res){
      console.log("Server has started 3000")
